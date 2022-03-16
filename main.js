94b05827fc442f55c06048716f55586f04c43355
erykwk <eryk.wojciuk@gmail.com>
2022-03-16 17:53:19 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 const btn = document.querySelector("button")
+let number = 1;
+
 const addElement = function () {
+    
     const newDiv = document.createElement("div")
+    newDiv.textContent = number;
+     if (number%5==0) {
+        newDiv.classList.add("circle")
+    }
     document.body.appendChild(newDiv)
+    number += 1
+   
     // console.log(newDiv)
 
 }
